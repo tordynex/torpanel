e797ab91d979b78640569fefcf9a425bdd36a9df
tordynex <tor@dynexperformance.se>
2025-08-04 13:13:03 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://www.portal.autonexo.se"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
